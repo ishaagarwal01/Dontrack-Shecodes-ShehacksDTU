dd703c61985b9d6fe2fbd72c2aef2ba4ca7b2579
laks1806 <67590424+laks1806@users.noreply.github.com>
2021-04-11 12:32:39 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,54 @@
+const serverInfo = {
+    url: 'https://conseil-edo.cryptonomic-infra.tech:443',
+    apiKey: 'f5ba3e1f-9971-4779-9037-fbd81bf4280f'
+
+
+function refreshTable(blocks = []) {
+    const tableRef = document.getElementById('query-table').getElementsByTagName('tbody')[0];
+    tableRef.innerHTML = '';
+    blocks.map((block, index) => {
+        const row = tableRef.insertRow(index);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        cell1.innerHTML = block.level;
+        cell2.innerHTML = new Date(block.timestamp).toLocaleString();
+        cell3.innerHTML = block.hash;
+    });
+}
+
+async function getBlocks(network, query) {
+    const blocks = await conseiljs.TezosConseilClient.getBlocks(serverInfo, network, query);
+    refreshTable(blocks);
+}
+
+function onClickGo(event) {
+    event.preventDefault();
+
+    const order = document.getElementById('order-select').value;
+    const limit = parseInt(document.getElementById('limit-input').value);
+
+    let query = conseiljs.ConseilQueryBuilder.blankQuery();
+    query = conseiljs.ConseilQueryBuilder.addFields(query, 'level', 'timestamp', 'hash');
+    query = conseiljs.ConseilQueryBuilder.addOrdering(query, 'level', order);
+    query = conseiljs.ConseilQueryBuilder.setLimit(query, limit);
+
+    getBlocks('carthagenet', query);
+}
+
+async function fetchMetadata() {
+    const platformResult = await conseiljs.ConseilMetadataClient.getPlatforms(serverInfo);
+    const platform = platformResult[0]['name'];
+
+    const networksResult = await conseiljs.ConseilMetadataClient.getNetworks(serverInfo, platform);
+    const network = networksResult[0]['name'];
+
+    const entityResult = await conseiljs.ConseilMetadataClient.getEntities(serverInfo, platform, network);
+    const blocks = Array.from(entityResult).filter(v => v['name'] === 'blocks')[0]['name'];
+
+    const attributeResult = await conseiljs.ConseilMetadataClient.getAttributes(serverInfo, platform, network, blocks);
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') { fetchMetadata(); }
+}
